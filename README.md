1e422e7498ee8dbcd38aa345c14491aca8dcc7d0
sgrant <sarahngrant0691@gmail.com>
2017-05-07 03:04:15 -0400

    Superuser created, style changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## README
 
-This is my second attempt at building a website using Django. This time I am using [Hello Web App](https://hellowebapp.com)
+This is my second attempt at building a website using Django. This time I am using [Hello Web App](https://hellowebapp.com).
--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+#def index(request):
+#    return render(request,'index.html')
+
 def index(request):
-    return render(request,'index.html')
+    # Define a variable to appear on the home page
+    podcast = 'my favorite murder'
+    number = 7
+    return render(request,'index.html',{'pc' : podcast, 'num' : number,})
