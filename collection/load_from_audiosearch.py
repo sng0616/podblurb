68a44c4e17e32cff4f0ccd3fad2fec7e9d54c36a
sgrant <sarahngrant0691@gmail.com>
2017-06-08 01:02:35 -0400

    Simple script added to make API calls to Audiosear.ch

--- collection/load_from_audiosearch.py
+++ collection/load_from_audiosearch.py
@@ -1,11 +1,18 @@
 #import os
-#import sys
-import requests
-import json
+import sys
+#import requests
+#import json
 import passwords as p
 
-from collection.models import podcast_show
+#from collection.models import podcast_show
 from audiosearch import Client
+
+show_title_search = ' '.join(sys.argv[1:])
+
+print show_title_search
 
 c = Client(p.audiosearch_oauth_id, p.audiosearch_oauth_secret)
 
+show_info = c.search({ 'q': show_title_search}, 'shows')
+
+print show_info
