23699899103be4a4b9ddc4e9373325139e5c9bef
sgrant <sarahngrant0691@gmail.com>
2017-06-09 02:59:55 -0400

    API request working, need to add serializers & save info to db

--- collection/load_from_audiosearch.py
+++ collection/load_from_audiosearch.py
@@ -12,22 +12,26 @@ results = c.search({ 'q': '*'}, 'shows')
 
 total_pages = int(m.floor(results['total_results']/results['results_per_page']))
 
-print results['total_results'], total_pages
+#print results['total_results'], total_pages
 
 all_shows = []
 
-for page in range(400,(total_pages+1)):
-    print page
-    page_results = c.search({ 'q': '*', 'page': page}, 'shows')
-    for i in page_results['results']:
-        if i['title'] not in all_shows:
-            all_shows.append(i['title'])
-            print i['title']
-        else:
-            pass
+for page in range(1,(total_pages+1)):
+#for page in range(1,5):
+#    print page
+    try:
+        page_results = c.search({ 'q': '*', 'page': page}, 'shows')
+        for i in page_results['results']:
+            if i['title'] not in all_shows:
+                show = [str(i['title'])]
+                show_string = tuple(show,)+tuple(show,)
+                all_shows.append(show_string)
+#                print i['title']
+            else:
+                pass
+    except ValueError:
+        pass
         
-
-
-#all_shows = sorted(all_shows)
-#print all_shows[0:15]
-print len(all_shows)
+all_shows = sorted(all_shows)
+#print all_shows[:5]
+#print len(all_shows)
--- collection/models.py
+++ collection/models.py
@@ -3,11 +3,38 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib import admin
 from taggit.managers import TaggableManager
+#from load_from_audiosearch import all_shows
+
+# Get show_name_choices
+import passwords as p
+import math as m
+from audiosearch import Client
+
+c = Client(p.audiosearch_oauth_id, p.audiosearch_oauth_secret)
+results = c.search({ 'q': '*'}, 'shows')
+total_pages = int(m.floor(results['total_results']/results['results_per_page']))
+
+all_shows = []
+
+for page in range(1,(total_pages+1)):
+    try:
+        page_results = c.search({ 'q': '*', 'page': page}, 'shows')
+        for i in page_results['results']:
+            if i['title'] not in all_shows:
+                show = [str(i['title'])]
+                show_string = tuple(show,)+tuple(show,)
+                all_shows.append(show_string)
+            else:
+                pass
+    except ValueError:
+        pass
+    
+all_shows = sorted(all_shows)
 
 # Create your models here.
 class podcast_show(models.Model):
-#    show_name_choices = ()
-    show_name = models.CharField(max_length=255)
+    show_name_choices = (all_shows)
+    show_name = models.CharField(max_length=255, choices = show_name_choices)
     show_description = models.TextField()
     show_website = models.CharField(max_length=100)
     show_slug = models.SlugField(unique=True)
